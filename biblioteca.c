2b1ec3921678a44ab14c94eab96e8f51ae28b13c
ALEXANDRE DOMICIANO PIERRI <unifapierri@feinet.fei.edu.br>
2023-09-29 18:50:59 -0300

    Criar função débito

--- biblioteca.c
+++ biblioteca.c
@@ -5,22 +5,24 @@
 #include <stdio.h>
 #include <string.h>
 
-int listaCliente(struct estadoPrograma state){
-    if(state.tamanho==0){
-        return;
+int listaCliente(struct estadoPrograma state) {
+    if (state.tamanho == 0) {
+        return -1;
     }
-    else{
+    else {
         for (int i = 0; i < state.tamanho; ++i) {
-            printf("Cliente %d\n",i);
-            printf("Nome: %s\n",state.memoria[i].nome);
-            printf("Tipo de conta: %d\n",state.memoria[i].tipo);
-            if(i==state.tamanho-1){
-                printf("CPF: %ld",state.memoria[i].cpf);
+            printf("Cliente %d\n", i);
+            printf("Nome: %s\n", state.memoria[i].nome);
+            printf("Tipo de conta: %d\n", state.memoria[i].tipo);
+            if (i == state.tamanho - 1) {
+                printf("CPF: %ld", state.memoria[i].cpf);
             }
-            else{printf("CPF: %ld\n",state.memoria[i].cpf);
+            else {
+                printf("CPF: %ld\n", state.memoria[i].cpf);
             }
         }
     }
+}
 
 int criarCliente(struct estadoPrograma *state){
     char nome[100];
@@ -74,6 +76,40 @@ int deletarCliente(struct estadoPrograma *state, long cpf){
     }
     else{
         rearranjarArray(&state, posicao);
+    }
+    return 0;
+}
+int debito( float valordeb,struct estadoPrograma*state){
+    long CPF;
+    printf("Digite o cpf do cliente:\n");
+    scanf("%ld", &CPF);
+    int pos=buscarCliente(*state,CPF);
+    if (pos==-1){
+        return -1;
+    }
+    else{
+        char senha[300];
+        printf("Digite a senha da conta:\n");
+        scanf("%300s", &senha);
+        printf("A senha eh %s\n",state->memoria[pos].senha);
+        if(strcmp(senha,state->memoria[pos].senha)!=0){
+            return -1;
+        }
+        if(state->memoria[pos].tipo==1){
+            printf("oi");
+            float totdeb=state->memoria[pos].valor-1.03*valordeb;
+            if(totdeb<=-5000){
+                return -1;
+                }
+            printf("Depois do dehbito de R$%.2f, sua conta tem com R$%.2f",valordeb,totdeb);
+            }
+        else{
+            float totdeb=state->memoria[pos].valor-1.05*valordeb;
+            if(totdeb<=-1000){
+                return -1;
+                }
+            printf("Depois do dehbito de R$%.2f, sua conta tem com R$%.2f",valordeb,totdeb);
+        }
     }
     return 0;
 }
